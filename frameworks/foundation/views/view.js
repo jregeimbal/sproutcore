5a4646e844e748f42a6030d7fe4fa719aac162c4
Mohammed Taher <mohammed.taher@eloqua.com>
2009-07-07 19:35:14 -0400

    trying to get orion to work in ie

--- frameworks/foundation/views/view.js
+++ frameworks/foundation/views/view.js
@@ -266,7 +266,10 @@ SC.View = SC.Responder.extend(SC.DelegateSupport,
       
       var childViews = this.get('childViews'), len = childViews.length, idx;
       for(idx=0;idx<len;idx++) {
-        childViews[idx].recomputeIsVisibleInWindow(cur);
+        var cv = childViews[idx];
+        if (cv) {
+          cv.recomputeIsVisibleInWindow(cur);
+        }
       }
         
       // if we just became visible, update layer + layout if needed...
@@ -1886,7 +1889,9 @@ SC.View = SC.Responder.extend(SC.DelegateSupport,
     var cvs = this.get('childViews'), len = cvs.length, idx, cv ;
     for (idx=0; idx<len; ++idx) {
       cv = cvs[idx] ;
-      if (!cv.hasStaticLayout) cv.notifyPropertyChange('clippingFrame') ;
+      if (cv) {
+        if (!cv.hasStaticLayout) cv.notifyPropertyChange('clippingFrame') ;
+      }
     }
   }.observes('clippingFrame'),
     
@@ -1931,7 +1936,10 @@ SC.View = SC.Responder.extend(SC.DelegateSupport,
     var cv = this.childViews, len = cv.length, idx, view ;
     for (idx=0; idx<len; ++idx) {
       view = cv[idx] ;
-      if (view.parentViewDidResize) view.parentViewDidResize() ;
+      if (view) {
+        if (view.parentViewDidResize) view.parentViewDidResize() ;
+      }
+      
     }
   }.observes('layout'),
   
