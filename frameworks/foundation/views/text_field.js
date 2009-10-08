ec115d5f1e11c81aee8c4ff4ef14a0db72af25c1
Mike Ball <mike.ball3@gmail.com>
2009-10-08 17:53:41 -0400

    Revert "Altered fieldValueDidChange function so that it would work in IE... it was previously throwing an error."
    This reverts commit 47081a902dccf4858e1343d5cc90abdfa15a9368.

--- frameworks/foundation/views/text_field.js
+++ frameworks/foundation/views/text_field.js
@@ -672,39 +672,5 @@ SC.TextFieldView = SC.FieldView.extend(SC.StaticLayout, SC.Editable,
 
   selectStart: function(evt) {
     return YES;
-  },
-  
-  fieldValueDidChange: function(partialChange) {
-    
-    // save the cursor's position
-    var element = this.$field()[0];
-    var start = element.selectionStart;
-    var end = element.selectionEnd;
-    
-    // restore the cursor's position
-    if (element.setSelectionRange) {
-      sc_super();
-      element.setSelectionRange(start, end);
-      
-    } else if (element.createTextRange){
-    	var bookmark = document.selection.createRange().getBookmark();
-    	var selection = element.createTextRange();
-    	selection.moveToBookmark(bookmark);
-
-    	var before = element.createTextRange();
-    	before.collapse(true);
-    	before.setEndPoint("EndToStart", selection);
-
-    	var beforeLength = before.text.length;
-    	var selLength = selection.text.length;
-    	
-    	sc_super();
-    	
-    	var range = element.createTextRange();
-    	range.collapse(true);
-    	range.moveStart('character', beforeLength);
-    	range.moveEnd('character', selLength);
-    	range.select();
-    }
   }
 }) ;
