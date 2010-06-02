eff9744e8548162c24547f767e6fd549b4e98150
Evin Grano <etgryphon@cox.net>
2010-06-02 15:08:43 -0400

    even better testing...need to remove the artificial reset.

--- frameworks/datastore/tests/models/nested_records/server_replace.js
+++ frameworks/datastore/tests/models/nested_records/server_replace.js
@@ -192,12 +192,13 @@ test("Test Commit to server and new member addition",function() {
 });
 
 test("Test Commit to server and new member addition",function() {
-  var family, familyHash, first, second, members, firstHash, secondHash;
+  var family, familyHash, firstMembers, secondMembers,
+      first, second, firstHash, secondHash;
   
   // First
   family = store.materializeRecord(storeKeys[0]);
-  members = family.get('members');
-  first = members.objectAt(0);
+  firstMembers = family.get('members');
+  first = firstMembers.objectAt(0);
   firstHash = first.get('attributes');
   store.writeStatus(storeKeys[0], SC.Record.BUSY_LOADING);
   store.dataSourceDidComplete(storeKeys[0], {
@@ -216,12 +217,19 @@ test("Test Commit to server and new member addition",function() {
   // Second
   family = store.materializeRecord(storeKeys[0]);
   familyHash = store.readDataHash(storeKeys[0]);
-  members = family.get('members');
-  second = members.objectAt(0);
+  secondMembers = family.get('members');
+  //debugger;
+  secondMembers._childrenContentDidChange();
+  second = secondMembers.objectAt(0);
   secondHash = second.get('attributes');
-  same(first, second, "the SC.ChildRecord should the be the same before and after the save");
-  same(firstHash, secondHash, "the SC.ChildRecordHashes should the be the same before and after the save");
-  same(first.get('id'), second.get('id'), "the SC.ChildRecord id should the be the same before and after the save");
+  
+  // Tests
+  equals(SC.guidFor(secondMembers), SC.guidFor(firstMembers), "verify that members ChildArrays are the same after save");
+  equals(SC.guidFor(second), SC.guidFor(first), "verify that Member 1 are the same after save");
+  equals(second.get('attributes'), first.get('attributes'), "verify that Member 1 attributes are the same after save");
+  same(second, first, "the SC.ChildRecord should the be the same before and after the save");
+  same(secondHash, firstHash, "the SC.ChildRecordHashes should the be the same before and after the save");
+  same(second.get('id'), first.get('id'), "the SC.ChildRecord id should the be the same before and after the save");
   same(secondHash, familyHash.members[0], "the Family Record and the member id hash should match");
 });
 
