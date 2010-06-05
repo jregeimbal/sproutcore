e71464b99b31f06604faa80a2c8d666ea01ea3d2
Mike Ball <mike.ball3@gmail.com>
2010-06-04 21:26:18 -0400

    make sure panes are not visible so they don't receive drag events and interfear with things...

--- frameworks/foundation/panes/pane.js
+++ frameworks/foundation/panes/pane.js
@@ -567,7 +567,7 @@ SC.Pane = SC.View.extend(SC.ResponderContext,
     var dom = this.get('layer') ;
     if (dom && dom.parentNode) dom.parentNode.removeChild(dom) ;
     dom = null ;
-    
+
     // remove intercept
     this._removeIntercept();
     
@@ -583,6 +583,7 @@ SC.Pane = SC.View.extend(SC.ResponderContext,
     // clean up some of my own properties
     this.set('isPaneAttached', NO) ;
     this.parentViewDidChange();
+    this.set('isVisible', NO);
     return this ;
   },
   
@@ -703,6 +704,7 @@ SC.Pane = SC.View.extend(SC.ResponderContext,
     this.set('currentWindowSize', responder.computeWindowSize()) ;
     
     // update my own location
+    this.set('isVisible', YES);
     this.set('isPaneAttached', YES) ;
     this.parentViewDidChange() ;
     
