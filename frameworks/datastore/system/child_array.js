e936710ae49adf60bc144bcf589a3beb8e9134f2
Evin Grano <etgryphon@cox.net>
2010-09-24 13:58:06 -0400

    Revert "hack for double nested child arrays"
    This reverts commit 926f350ef3151812c5d1412fbbeb7d7019cf4494.

--- frameworks/datastore/system/child_array.js
+++ frameworks/datastore/system/child_array.js
@@ -223,13 +223,6 @@ SC.ChildArray = SC.Object.extend(SC.Enumerable, SC.Array,
     
     var rev = (children) ? children.propertyRevision : -1 ;
     this._childrenContentDidChange(children, '[]', children, rev);
-    
-    //HACK: [MB/EG] shouldn't flush the child arrays contents in a range updater
-    this.forEach(function(child){
-      child.storeDidChangeProperties();
-    });
-    //end HACK
-    
   },
 
   /** @private
