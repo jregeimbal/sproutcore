b7318196a20e5e9f8f5279e1827deb46eb744b4d
geoffreyd <geoffreyd@gmail.com>
2010-10-12 19:56:03 +1000

    added tests for queryWithScope to ensure correct behaviour.

--- frameworks/datastore/tests/system/query/queryWithScope.js
+++ frameworks/datastore/tests/system/query/queryWithScope.js
@@ -3,13 +3,29 @@
 // Copyright: ©2006-2009 Apple Inc. and contributors.
 // License:   Licensed under MIT license (see license.js)
 // ==========================================================================
-/*globals module ok equals same test MyApp */
+/*globals module ok equals same test myApp */
  
 // test parsing of query string
-var q, scope1, scope2;
+var myApp = {}, scope1, scope2;
 module("SC.Query#queryWithScope", {
   setup: function() {
-    q = SC.Query.create({
+
+    myApp = SC.Object.create({
+      Person: SC.Record.extend({
+        firstName: SC.Record.attr(String),
+        lastName: SC.Record.attr(String)
+      }),
+      store: SC.Store.create().from(SC.DataSource.create())
+    });
+    myApp.PEOPLE_QUERY = SC.Query.local(myApp.Person);
+    myApp.WHITE_QUERY  = SC.Query.local(myApp.Person, 'lastName = "White"');
+
+    SC.RunLoop.begin();
+    myApp.store.pushRetrieve(myApp.Person, 1, {firstName: 'Bo', lastName: 'Jeanes'});
+    myApp.store.pushRetrieve(myApp.Person, 2, {firstName: 'Jo', lastName: 'White'});
+    SC.RunLoop.end();
+
+    myApp.q = SC.Query.create({
       conditions: "foo = bar",
       parameters: { foo: "bar" },
       orderBy: "foo",
@@ -23,7 +39,7 @@ module("SC.Query#queryWithScope", {
   },
   
   teardown: function() {
-    q = scope1 = scope2 = null;
+    myApp = scope1 = scope2 = null;
   }
 });
 
@@ -36,16 +52,16 @@ function verifyCopy(copy, original) {
 
 test("getting into scope first time", function() {
   
-  var q2 = q.queryWithScope(scope1);
-  verifyCopy(q2, q);
+  var q2 = myApp.q.queryWithScope(scope1);
+  verifyCopy(q2, myApp.q);
   equals(q2.get('scope'), scope1, 'new query should have scope1');
   
-  var q3 = q.queryWithScope(scope1);
+  var q3 = myApp.q.queryWithScope(scope1);
   equals(q3, q2, 'calling again with same scope should return same instance');
 });
 
 test("chained scope", function() {
-  var q2 = q.queryWithScope(scope1) ;
+  var q2 = myApp.q.queryWithScope(scope1) ;
   var q3 = q2.queryWithScope(scope2);
   
   verifyCopy(q3, q2);
@@ -55,3 +71,23 @@ test("chained scope", function() {
   equals(q4, q3, 'calling again with same scope should return same instance');
 });
 
+test("Scoped Query Should return correct results before and after store updates", function(){
+  var people = myApp.store.find(myApp.PEOPLE_QUERY),
+      white  = myApp.store.find(myApp.WHITE_QUERY.queryWithScope(people));
+
+  equals(people.get('length'), 2, "should have found 2 records");
+  ok(people.getEach('firstName').isEqual(["Bo", "Jo"]), "FirstNames of all people");
+
+  equals(white.get('length'), 1, "should only find 1 white");
+  equals(white.getEach('firstName').toString(), (["Jo"]).toString(), "FirstNames of the white");
+
+  SC.RunLoop.begin();
+  myApp.store.pushRetrieve(myApp.Person, 3, {firstName: 'Alex', lastName: 'White'});
+  SC.RunLoop.end();
+
+  equals(people.get('length'), 3, "should have found 3 records");
+  ok(people.getEach('firstName').isEqual(["Bo", "Jo", "Alex"]), "FirstNames of all people");
+
+  equals(white.get('length'), 2, "should only find 2 whites");
+  equals(white.getEach('firstName').toString(), (["Jo", "Alex"]).toString(), "FirstNames of all whites");
+});
