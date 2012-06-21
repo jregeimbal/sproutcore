3f6f1b313ab39a40cc9fb3036cb7fdccdfa66c8d
Joe Shelby <joe.shelby@eloqua.com>
2012-06-21 12:32:50 -0400

    revert OEP-2382 - breaks TextField and hints and value change events in a major way.

--- frameworks/foundation/views/text_field.js
+++ frameworks/foundation/views/text_field.js
@@ -520,7 +520,7 @@ SC.TextFieldView = SC.FieldView.extend(SC.StaticLayout, SC.Editable,
       var input= this.$input();
       if(!this.get('_supportsPlaceHolder')){
         var val = this.get('value');
-        if((SC.empty(val) || (val && val.length===0))){
+        if((!val || (val && val.length===0))){
           if(this.get('hintON') && !this.get('isFirstResponder')){
             //console.log('hint on render');
             context.setClass('sc-hint', YES);
