b75715b78a35cc8a44f400a54c6efea021451f65
Joe Shelby <joe.shelby@eloqua.com>
2013-06-13 16:04:03 -0400

    Fixing date rendering for 463, with FIXME to address this properly in 464.

--- frameworks/foundation/english.lproj/strings.js
+++ frameworks/foundation/english.lproj/strings.js
@@ -6,8 +6,9 @@
 // ==========================================================================
 
 sc_require('system/locale');
-
-SC.stringsFor('English', {
+// FIXME: [JS][CC][AD][I18N] we need to deal with these in a multi-language manner without the need to add every
+// language to sproutcore.  this was "English", but the locale for E10 is now en-us
+SC.stringsFor('en-us', {
   '_SC.DateTime.dayNames': 'Sunday Monday Tuesday Wednesday Thursday Friday Saturday',
   '_SC.DateTime.abbreviatedDayNames': 'Sun Mon Tue Wed Thu Fri Sat',
   '_SC.DateTime.monthNames': 'January February March April May June July August September October November December',
