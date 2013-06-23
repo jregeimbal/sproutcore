f5bc8529a11d7e9a7586b3e3ae2f8915c8445254
Colin Coller <colin.coller@eloqua.com>
2013-06-23 14:30:40 -0400

    Adding en.lproj to fix tests until we can switch the build from /en/ to /en-us/.

---
+++ frameworks/foundation/languages/en.lproj/strings.js
@@ -0,0 +1,249 @@
+sc_require('system/locale_setup');
+
+// Strings for English locales.
+// 
+// This file was generated based on information in the Microsoft .NET
+// framework.  Do not edit it by hand.
+
+// English (United States)
+SC.stringsFor('en-us', {
+  '_SC.DateTime.dayNames': 'Sunday Monday Tuesday Wednesday Thursday Friday Saturday',
+  '_SC.DateTime.abbreviatedDayNames': 'Sun Mon Tue Wed Thu Fri Sat',
+  '_SC.DateTime.monthNames': 'January February March April May June July August September October November December',
+  '_SC.DateTime.abbreviatedMonthNames': 'Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec',
+  '_SC.DateTime.amDesignator': 'AM',
+  '_SC.DateTime.pmDesignator': 'PM',
+  '_SC.DateTime.shortDatePattern': '%-m/%-d/%Y',
+  '_SC.DateTime.shortDateLabel': 'MM/DD/YYYY',
+  '_SC.DateTime.shortDateExample': '7/13/2013',
+  '_SC.DateTime.shortTimePattern': '%i:%M %p',
+  '_SC.DateTime.shortTimeLabel': 'HH:MM AM/PM',
+  '_SC.DateTime.shortTimeExample': '2:30 PM',
+  '_SC.DateTime.shortDateShortTimePattern': '%-m/%-d/%Y %i:%M %p',
+  '_SC.DateTime.shortDateShortTimeLabel': 'MM/DD/YYYY HH:MM AM/PM',
+  '_SC.DateTime.shortDateShortTimeExample': '7/13/2013 2:30 PM',
+  '_SC.DateTime.longDatePattern': '%A, %B %d, %Y',
+  '_SC.DateTime.longTimePattern': '%i:%M:%S %p',
+  '_SC.DateTime.shortDateLongTimePattern': '%-m/%-d/%Y %i:%M:%S %p',
+  '_SC.DateTime.longDateShortTimePattern': '%A, %B %d, %Y %i:%M %p',
+  '_SC.DateTime.longDateLongTimePattern': '%A, %B %d, %Y %i:%M:%S %p',
+});
+
+// English
+SC.stringsFor('en', {
+});
+
+// English (United Kingdom)
+SC.stringsFor('en-gb', {
+  '_SC.DateTime.shortDatePattern': '%d/%m/%Y',
+  '_SC.DateTime.shortDateLabel': 'DD/MM/YYYY',
+  '_SC.DateTime.shortDateExample': '13/07/2013',
+  '_SC.DateTime.shortTimePattern': '%H:%M',
+  '_SC.DateTime.shortTimeLabel': 'HH:MM',
+  '_SC.DateTime.shortTimeExample': '14:30',
+  '_SC.DateTime.shortDateShortTimePattern': '%d/%m/%Y %H:%M',
+  '_SC.DateTime.shortDateShortTimeLabel': 'DD/MM/YYYY HH:MM',
+  '_SC.DateTime.shortDateShortTimeExample': '13/07/2013 14:30',
+  '_SC.DateTime.longDatePattern': '%d %B %Y',
+  '_SC.DateTime.longTimePattern': '%H:%M:%S',
+  '_SC.DateTime.shortDateLongTimePattern': '%d/%m/%Y %H:%M:%S',
+  '_SC.DateTime.longDateShortTimePattern': '%d %B %Y %H:%M',
+  '_SC.DateTime.longDateLongTimePattern': '%d %B %Y %H:%M:%S',
+});
+
+// English (Australia)
+SC.stringsFor('en-au', {
+  '_SC.DateTime.shortDatePattern': '%-d/%m/%Y',
+  '_SC.DateTime.shortDateLabel': 'DD/MM/YYYY',
+  '_SC.DateTime.shortDateExample': '13/07/2013',
+  '_SC.DateTime.shortDateShortTimePattern': '%-d/%m/%Y %i:%M %p',
+  '_SC.DateTime.shortDateShortTimeLabel': 'DD/MM/YYYY HH:MM AM/PM',
+  '_SC.DateTime.shortDateShortTimeExample': '13/07/2013 2:30 PM',
+  '_SC.DateTime.longDatePattern': '%A, %-d %B %Y',
+  '_SC.DateTime.shortDateLongTimePattern': '%-d/%m/%Y %i:%M:%S %p',
+  '_SC.DateTime.longDateShortTimePattern': '%A, %-d %B %Y %i:%M %p',
+  '_SC.DateTime.longDateLongTimePattern': '%A, %-d %B %Y %i:%M:%S %p',
+});
+
+// English (Canada)
+SC.stringsFor('en-ca', {
+  '_SC.DateTime.shortDatePattern': '%d/%m/%Y',
+  '_SC.DateTime.shortDateLabel': 'DD/MM/YYYY',
+  '_SC.DateTime.shortDateExample': '13/07/2013',
+  '_SC.DateTime.shortDateShortTimePattern': '%d/%m/%Y %i:%M %p',
+  '_SC.DateTime.shortDateShortTimeLabel': 'DD/MM/YYYY HH:MM AM/PM',
+  '_SC.DateTime.shortDateShortTimeExample': '13/07/2013 2:30 PM',
+  '_SC.DateTime.longDatePattern': '%B-%d-%y',
+  '_SC.DateTime.shortDateLongTimePattern': '%d/%m/%Y %i:%M:%S %p',
+  '_SC.DateTime.longDateShortTimePattern': '%B-%d-%y %i:%M %p',
+  '_SC.DateTime.longDateLongTimePattern': '%B-%d-%y %i:%M:%S %p',
+});
+
+// English (New Zealand)
+SC.stringsFor('en-nz', {
+  '_SC.DateTime.amDesignator': 'a.m.',
+  '_SC.DateTime.pmDesignator': 'p.m.',
+  '_SC.DateTime.shortDatePattern': '%-d/%m/%Y',
+  '_SC.DateTime.shortDateLabel': 'DD/MM/YYYY',
+  '_SC.DateTime.shortDateExample': '13/07/2013',
+  '_SC.DateTime.shortTimeLabel': 'HH:MM a.m./p.m.',
+  '_SC.DateTime.shortTimeExample': '2:30 p.m.',
+  '_SC.DateTime.shortDateShortTimePattern': '%-d/%m/%Y %i:%M %p',
+  '_SC.DateTime.shortDateShortTimeLabel': 'DD/MM/YYYY HH:MM a.m./p.m.',
+  '_SC.DateTime.shortDateShortTimeExample': '13/07/2013 2:30 p.m.',
+  '_SC.DateTime.longDatePattern': '%A, %-d %B %Y',
+  '_SC.DateTime.shortDateLongTimePattern': '%-d/%m/%Y %i:%M:%S %p',
+  '_SC.DateTime.longDateShortTimePattern': '%A, %-d %B %Y %i:%M %p',
+  '_SC.DateTime.longDateLongTimePattern': '%A, %-d %B %Y %i:%M:%S %p',
+});
+
+// English (Ireland)
+SC.stringsFor('en-ie', {
+  '_SC.DateTime.amDesignator': '',
+  '_SC.DateTime.pmDesignator': '',
+  '_SC.DateTime.shortDatePattern': '%d/%m/%Y',
+  '_SC.DateTime.shortDateLabel': 'DD/MM/YYYY',
+  '_SC.DateTime.shortDateExample': '13/07/2013',
+  '_SC.DateTime.shortTimePattern': '%H:%M',
+  '_SC.DateTime.shortTimeLabel': 'HH:MM',
+  '_SC.DateTime.shortTimeExample': '14:30',
+  '_SC.DateTime.shortDateShortTimePattern': '%d/%m/%Y %H:%M',
+  '_SC.DateTime.shortDateShortTimeLabel': 'DD/MM/YYYY HH:MM',
+  '_SC.DateTime.shortDateShortTimeExample': '13/07/2013 14:30',
+  '_SC.DateTime.longDatePattern': '%d %B %Y',
+  '_SC.DateTime.longTimePattern': '%H:%M:%S',
+  '_SC.DateTime.shortDateLongTimePattern': '%d/%m/%Y %H:%M:%S',
+  '_SC.DateTime.longDateShortTimePattern': '%d %B %Y %H:%M',
+  '_SC.DateTime.longDateLongTimePattern': '%d %B %Y %H:%M:%S',
+});
+
+// English (South Africa)
+SC.stringsFor('en-za', {
+  '_SC.DateTime.shortDatePattern': '%Y/%m/%d',
+  '_SC.DateTime.shortDateLabel': 'YYYY/MM/DD',
+  '_SC.DateTime.shortDateExample': '2013/07/13',
+  '_SC.DateTime.shortTimePattern': '%I:%M %p',
+  '_SC.DateTime.shortTimeExample': '02:30 PM',
+  '_SC.DateTime.shortDateShortTimePattern': '%Y/%m/%d %I:%M %p',
+  '_SC.DateTime.shortDateShortTimeLabel': 'YYYY/MM/DD HH:MM AM/PM',
+  '_SC.DateTime.shortDateShortTimeExample': '2013/07/13 02:30 PM',
+  '_SC.DateTime.longDatePattern': '%d %B %Y',
+  '_SC.DateTime.longTimePattern': '%I:%M:%S %p',
+  '_SC.DateTime.shortDateLongTimePattern': '%Y/%m/%d %I:%M:%S %p',
+  '_SC.DateTime.longDateShortTimePattern': '%d %B %Y %I:%M %p',
+  '_SC.DateTime.longDateLongTimePattern': '%d %B %Y %I:%M:%S %p',
+});
+
+// English (Jamaica)
+SC.stringsFor('en-jm', {
+  '_SC.DateTime.shortDatePattern': '%d/%m/%Y',
+  '_SC.DateTime.shortDateLabel': 'DD/MM/YYYY',
+  '_SC.DateTime.shortDateExample': '13/07/2013',
+  '_SC.DateTime.shortTimePattern': '%I:%M %p',
+  '_SC.DateTime.shortTimeExample': '02:30 PM',
+  '_SC.DateTime.shortDateShortTimePattern': '%d/%m/%Y %I:%M %p',
+  '_SC.DateTime.shortDateShortTimeLabel': 'DD/MM/YYYY HH:MM AM/PM',
+  '_SC.DateTime.shortDateShortTimeExample': '13/07/2013 02:30 PM',
+  '_SC.DateTime.longTimePattern': '%I:%M:%S %p',
+  '_SC.DateTime.shortDateLongTimePattern': '%d/%m/%Y %I:%M:%S %p',
+  '_SC.DateTime.longDateShortTimePattern': '%A, %B %d, %Y %I:%M %p',
+  '_SC.DateTime.longDateLongTimePattern': '%A, %B %d, %Y %I:%M:%S %p',
+});
+
+// English (Caribbean)
+SC.stringsFor('en-029', {
+  '_SC.DateTime.shortDatePattern': '%m/%d/%Y',
+  '_SC.DateTime.shortDateExample': '07/13/2013',
+  '_SC.DateTime.shortDateShortTimePattern': '%m/%d/%Y %i:%M %p',
+  '_SC.DateTime.shortDateShortTimeExample': '07/13/2013 2:30 PM',
+  '_SC.DateTime.shortDateLongTimePattern': '%m/%d/%Y %i:%M:%S %p',
+});
+
+// English (Belize)
+SC.stringsFor('en-bz', {
+  '_SC.DateTime.shortDatePattern': '%d/%m/%Y',
+  '_SC.DateTime.shortDateLabel': 'DD/MM/YYYY',
+  '_SC.DateTime.shortDateExample': '13/07/2013',
+  '_SC.DateTime.shortTimePattern': '%I:%M %p',
+  '_SC.DateTime.shortTimeExample': '02:30 PM',
+  '_SC.DateTime.shortDateShortTimePattern': '%d/%m/%Y %I:%M %p',
+  '_SC.DateTime.shortDateShortTimeLabel': 'DD/MM/YYYY HH:MM AM/PM',
+  '_SC.DateTime.shortDateShortTimeExample': '13/07/2013 02:30 PM',
+  '_SC.DateTime.longDatePattern': '%A, %d %B %Y',
+  '_SC.DateTime.longTimePattern': '%I:%M:%S %p',
+  '_SC.DateTime.shortDateLongTimePattern': '%d/%m/%Y %I:%M:%S %p',
+  '_SC.DateTime.longDateShortTimePattern': '%A, %d %B %Y %I:%M %p',
+  '_SC.DateTime.longDateLongTimePattern': '%A, %d %B %Y %I:%M:%S %p',
+});
+
+// English (Trinidad and Tobago)
+SC.stringsFor('en-tt', {
+  '_SC.DateTime.shortDatePattern': '%d/%m/%Y',
+  '_SC.DateTime.shortDateLabel': 'DD/MM/YYYY',
+  '_SC.DateTime.shortDateExample': '13/07/2013',
+  '_SC.DateTime.shortTimePattern': '%I:%M %p',
+  '_SC.DateTime.shortTimeExample': '02:30 PM',
+  '_SC.DateTime.shortDateShortTimePattern': '%d/%m/%Y %I:%M %p',
+  '_SC.DateTime.shortDateShortTimeLabel': 'DD/MM/YYYY HH:MM AM/PM',
+  '_SC.DateTime.shortDateShortTimeExample': '13/07/2013 02:30 PM',
+  '_SC.DateTime.longDatePattern': '%A, %d %B %Y',
+  '_SC.DateTime.longTimePattern': '%I:%M:%S %p',
+  '_SC.DateTime.shortDateLongTimePattern': '%d/%m/%Y %I:%M:%S %p',
+  '_SC.DateTime.longDateShortTimePattern': '%A, %d %B %Y %I:%M %p',
+  '_SC.DateTime.longDateLongTimePattern': '%A, %d %B %Y %I:%M:%S %p',
+});
+
+// English (Zimbabwe)
+SC.stringsFor('en-zw', {
+});
+
+// English (Republic of the Philippines)
+SC.stringsFor('en-ph', {
+});
+
+// English (India)
+SC.stringsFor('en-in', {
+  '_SC.DateTime.shortDatePattern': '%d-%m-%Y',
+  '_SC.DateTime.shortDateLabel': 'DD-MM-YYYY',
+  '_SC.DateTime.shortDateExample': '13-07-2013',
+  '_SC.DateTime.shortTimePattern': '%H:%M',
+  '_SC.DateTime.shortTimeLabel': 'HH:MM',
+  '_SC.DateTime.shortTimeExample': '14:30',
+  '_SC.DateTime.shortDateShortTimePattern': '%d-%m-%Y %H:%M',
+  '_SC.DateTime.shortDateShortTimeLabel': 'DD-MM-YYYY HH:MM',
+  '_SC.DateTime.shortDateShortTimeExample': '13-07-2013 14:30',
+  '_SC.DateTime.longDatePattern': '%d %B %Y',
+  '_SC.DateTime.longTimePattern': '%H:%M:%S',
+  '_SC.DateTime.shortDateLongTimePattern': '%d-%m-%Y %H:%M:%S',
+  '_SC.DateTime.longDateShortTimePattern': '%d %B %Y %H:%M',
+  '_SC.DateTime.longDateLongTimePattern': '%d %B %Y %H:%M:%S',
+});
+
+// English (Malaysia)
+SC.stringsFor('en-my', {
+  '_SC.DateTime.shortDatePattern': '%-d/%-m/%Y',
+  '_SC.DateTime.shortDateLabel': 'DD/MM/YYYY',
+  '_SC.DateTime.shortDateExample': '13/7/2013',
+  '_SC.DateTime.shortDateShortTimePattern': '%-d/%-m/%Y %i:%M %p',
+  '_SC.DateTime.shortDateShortTimeLabel': 'DD/MM/YYYY HH:MM AM/PM',
+  '_SC.DateTime.shortDateShortTimeExample': '13/7/2013 2:30 PM',
+  '_SC.DateTime.longDatePattern': '%A, %-d %B, %Y',
+  '_SC.DateTime.shortDateLongTimePattern': '%-d/%-m/%Y %i:%M:%S %p',
+  '_SC.DateTime.longDateShortTimePattern': '%A, %-d %B, %Y %i:%M %p',
+  '_SC.DateTime.longDateLongTimePattern': '%A, %-d %B, %Y %i:%M:%S %p',
+});
+
+// English (Singapore)
+SC.stringsFor('en-sg', {
+  '_SC.DateTime.shortDatePattern': '%-d/%-m/%Y',
+  '_SC.DateTime.shortDateLabel': 'DD/MM/YYYY',
+  '_SC.DateTime.shortDateExample': '13/7/2013',
+  '_SC.DateTime.shortDateShortTimePattern': '%-d/%-m/%Y %i:%M %p',
+  '_SC.DateTime.shortDateShortTimeLabel': 'DD/MM/YYYY HH:MM AM/PM',
+  '_SC.DateTime.shortDateShortTimeExample': '13/7/2013 2:30 PM',
+  '_SC.DateTime.longDatePattern': '%A, %-d %B, %Y',
+  '_SC.DateTime.shortDateLongTimePattern': '%-d/%-m/%Y %i:%M:%S %p',
+  '_SC.DateTime.longDateShortTimePattern': '%A, %-d %B, %Y %i:%M %p',
+  '_SC.DateTime.longDateLongTimePattern': '%A, %-d %B, %Y %i:%M:%S %p',
+});
+
