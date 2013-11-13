3f0e209cc2cfa2bd019ec16cb5ea38f351a19691
Joe Shelby <joe.shelby@eloqua.com>
2013-11-13 17:53:33 -0500

    make "id" days of week bigger for testing.

--- frameworks/foundation/languages/id.lproj/strings-datetime.js
+++ frameworks/foundation/languages/id.lproj/strings-datetime.js
@@ -7,7 +7,7 @@ sc_require('system/locale_setup');
 // Indonesian
 SC.stringsFor('id', {
   '_SC.DateTime.dayNames': 'Sunday Monday Tuesday Wednesday Thursday Friday Saturday',
-  '_SC.DateTime.abbreviatedDayNames': 'Sun Mon Tue Wed Thu Fri Sat',
+  '_SC.DateTime.abbreviatedDayNames': 'dim. lun. mar. mer. jeu. ven. sam.',
   '_SC.DateTime.monthNames': 'January February March April May June July August September October November December',
   '_SC.DateTime.abbreviatedMonthNames': 'Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec',
   '_SC.DateTime.amDesignator': 'a.m.',
